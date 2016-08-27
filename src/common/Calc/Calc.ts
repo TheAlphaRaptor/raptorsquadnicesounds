65b0aa6fb5ed9abea9704ece39d1e6eea8aa0c6d
kkuhn <kkuhn0000@hotmail.com>
2016-08-27 11:02:19 -0700

    AKSdhoaisfbuoaishfoiasfjkasjifklasjfpashpofapsifp

--- src/common/Calc/Calc.ts
+++ src/common/Calc/Calc.ts
@@ -2,8 +2,13 @@
 
 export default class Calc {
 
-     Mult(x: number , y: number, z: number) {
-         return x * y * z;
+     Mult(...Kappa: number[]) {
+        var sum = 1;
+        for (var i = 0; i < Kappa.length; ++i)
+        {
+            sum *= Kappa[i];
+        }
+return sum;
     }
      Add(x: number , y:number , z:number) {
          return x + y + z;
--- src/init.ts
+++ src/init.ts
@@ -4,5 +4,5 @@ var c = new Calc;
 
 console.log(c.Add( 4 ,5 , 6));
 console.log(c.Sub( 4 ,5 , 6));
-console.log(c.Mult( 4 ,5 , 6));
+console.log(c.Mult(4 , 5, 6, 7, 8));
 console.log(c.Div( 4 ,5 , 6));
